8b58a1c977b9e506ca2969d55be78b4582ca44c5
Hchan2 <39434913+Hschan2@users.noreply.github.com>
2020-05-29 22:35:55 +0900

    Create 평균 점수.c

---
+++ 실습 1/평균 점수.c
@@ -0,0 +1,16 @@
+int main() {
+    int a[5] = {0}, sum = 0;  
+    
+    for(int i=0;i<5;i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    for(int i=0; i<5; i++) {
+        if(a[i] < 40) {
+            a[i] = 40;
+        }
+        sum += a[i];
+    }
+    
+    printf("%d", sum / 5);
+}
