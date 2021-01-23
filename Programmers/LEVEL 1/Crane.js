14efd98aa0fd7e0c206ffb05c06f93cac5611ade
HONGSEONGCHAN <seongchan_@naver.com>
2021-01-23 13:16:37 +0900

    크레인 인형뽑기 게임

--- Programmers/LEVEL 1/Crane.js
+++ Programmers/LEVEL 1/Crane.js
@@ -66,3 +66,32 @@ function solution(board, moves) {
     
     return count;
 }
+
+
+// ---------------------------------------------------------------------------
+
+function solution(board, moves) {
+    let count = 0;
+    let stack = [];
+    let lastStackItem = 0;
+    
+    for(let i = 0; i < moves.length; i++) {
+        for(let j = 0; j < board.length; j++) {
+            if(board[j][moves[i] - 1] !== 0){
+                lastStackItem = stack.length - 1;
+                
+                if(board[j][moves[i] - 1] === stack[lastStackItem]) {
+                    stack.pop();
+                    count += 2;
+                } else {
+                    stack.push(board[j][moves[i] - 1]);
+                }
+                
+                board[j][moves[i] - 1] = 0;
+                break;
+            }
+        }
+    }
+    
+    return count;
+}
